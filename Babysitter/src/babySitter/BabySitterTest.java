ae0698e97fb00dec9e76d7fd448e36e3eeb74769
bryandulaney <bryan.c.dulaney@gmail.com>
2015-06-15 19:21:34 -0400

    Third test -- calculate wage before bedtime.

--- Babysitter/src/babySitter/BabySitterTest.java
+++ Babysitter/src/babySitter/BabySitterTest.java
@@ -5,20 +5,27 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BabySitterTest {
-
 	
+	private static final int BEDTIME = 8;
+
 	@Test
 	public void shouldNotStartBefore5PM() {
-		Babysitter sitter = new Babysitter(2,0,0);
+		Babysitter sitter = new Babysitter(2, 0, BEDTIME);
 		assertEquals(false, sitter.eligibleStartTime(2));
 		assertEquals(true, sitter.eligibleStartTime(5));
 	}
-	
+
 	@Test
 	public void shouldNotEndAfter4AM() {
-		Babysitter sitter = new Babysitter(0,17,0);
+		Babysitter sitter = new Babysitter(0, 17, BEDTIME);
 		assertEquals(false, sitter.eligibleEndTime(17));
 		assertEquals(true, sitter.eligibleEndTime(16));
+	}
+	
+	@Test
+	public void shouldMake12DollarsPerHourBeforeBedtime() {
+		Babysitter sitter = new Babysitter(6,8,BEDTIME);
+		assertEquals(24, sitter.calcPay());
 	}
 
 }
--- Babysitter/src/babySitter/Babysitter.java
+++ Babysitter/src/babySitter/Babysitter.java
@@ -7,23 +7,22 @@ public class Babysitter {
 	private static final int PAY_BEFORE_BEDTIME = 12;
 	private static final int PAY_FROM_BEDTIME_TILL_MIDNIGHT = 8;
 	private static final int PAY_AFTER_MIDNIGHT = 16;
-	
+
 	public Babysitter(int startTime, int endTime, int bedTime) {
 		start = startTime;
 		end = endTime;
 		bed = bedTime;
 	}
-	
-	
+
 	public boolean eligibleStartTime(int i) {
 		start = i;
 		if (start >= 5) {
 			return true;
 		} else {
-		return false;
+			return false;
 		}
 	}
-	
+
 	public boolean eligibleEndTime(int i) {
 		end = i;
 		if (end <= 16) {
@@ -31,5 +30,24 @@ public class Babysitter {
 		} else {
 			return false;
 		}
+	}
+
+	public int calcPay() {
+		int hourlyWage = 0;
+		int totalWage = 0;
+
+		for (int currentTime = start; currentTime < end; currentTime++) {
+			if (isBeforeBedtime(currentTime)) {
+				hourlyWage = PAY_BEFORE_BEDTIME;
+			}
+
+			totalWage += hourlyWage;
+			hourlyWage = 0;
+		}
+		return totalWage;
+	}
+
+	private boolean isBeforeBedtime(int hour) {
+		return hour < bed;
 	}
 }
