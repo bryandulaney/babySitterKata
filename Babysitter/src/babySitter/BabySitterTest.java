8253b8b63107171d3c797523524cf1888faf6df7
bryandulaney <bryan.c.dulaney@gmail.com>
2015-06-15 15:03:46 -0400

    Intitial check-in of Babysitter kata.

---
+++ Babysitter/src/babySitter/BabySitterTest.java
@@ -0,0 +1,14 @@
+package babySitter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BabySitterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Babysitter/src/babySitter/Babysitter.java
@@ -0,0 +1,5 @@
+package babySitter;
+
+public class Babysitter {
+
+}
