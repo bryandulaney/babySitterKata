6fa2f67680eb79a6d7550d479040b1df988e1c6a
bryandulaney <bryan.c.dulaney@gmail.com>
2015-07-16 15:28:39 -0400

    refactor: wrote new test 'bedtimeIsStartTime' to test an edge case of bedtime to midnight pay and midnight to 4am pay.

--- Babysitter/src/babySitter/BabySitterTest.java
+++ Babysitter/src/babySitter/BabySitterTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BabySitterTest {
-	
+
 	private static final int BEDTIME = 8;
 
 	@Test
@@ -19,30 +19,35 @@ public class BabySitterTest {
 		Babysitter sitter = new Babysitter(16, 18, BEDTIME);
 		assertEquals(0, sitter.calcPay());
 	}
-	
+
 	@Test
 	public void shouldMake12DollarsPerHourBeforeBedtime() {
-		Babysitter sitter = new Babysitter(6,8,BEDTIME);
+		Babysitter sitter = new Babysitter(6, 8, BEDTIME);
 		assertEquals(24, sitter.calcPay());
 	}
-	
+
 	@Test
 	public void shouldMake8DollarsPerHourFromBedtimeTilMidnight() {
 		Babysitter sitter = new Babysitter(8, 10, BEDTIME);
 		assertEquals(16, sitter.calcPay());
 	}
-	
+
 	@Test
 	public void shouldMake16DollarsPerHourFromMidnightTilEndOfShift() {
 		Babysitter sitter = new Babysitter(12, 14, BEDTIME);
 		assertEquals(32, sitter.calcPay());
 	}
-	
+
 	@Test
 	public void shouldBePaidCorrectlyAtEndOfShift() {
 		Babysitter sitter = new Babysitter(5, 16, BEDTIME);
 		assertEquals(132, sitter.calcPay());
 	}
 
+	@Test
+	public void bedtimeIsStartTime() {
+		Babysitter sitter = new Babysitter(BEDTIME, 16, BEDTIME);
+		assertEquals(96, sitter.calcPay());
+	}
 
 }
